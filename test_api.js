1eb8d4e9979eb73ad3516dbfd07aeb9b6eaf3c38
Arju <thapaarju11@gmail.com>
2026-05-27 15:25:32 +0545

    clean: remove temporary/unrelated scratch and test files

--- test_api.js
+++
@@ -1,2 +0,0 @@
-const axios = require('axios');
-axios.get('http://localhost:3001/api/products').then(res => console.log('Products:', res.data.length)).catch(err => console.error('Error:', err.message));
